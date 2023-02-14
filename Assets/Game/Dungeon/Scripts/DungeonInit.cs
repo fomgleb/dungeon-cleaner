3e2d7824667df2be705a0810a98276ad00c097e5
fomgleb <fomglib@gmail.com>
2023-02-14 18:46:45 +0300

    Move Cave, Torches and Enemies Generators to DungeonInit

--- Assets/Game/Dungeon/Scripts/DungeonInit.cs
+++ Assets/Game/Dungeon/Scripts/DungeonInit.cs
@@ -5,11 +5,17 @@ namespace Game.Dungeon.Scripts
 {
     public class DungeonInit : MonoBehaviour
     {
+        [SerializeField] private DungeonGeneratorBase caveGenerator;
+        [SerializeField] private RandomTorchesInDungeonGenerator torchesGenerator;
+        [SerializeField] private RandomEnemiesSpawner enemiesSpawner;
         [SerializeField] private GameObjectSpawner playerSpawner;
 
         private void Start()
         {
             Pauser.ClearRegisteredHandlers();
+            caveGenerator._GenerateDungeon();
+            torchesGenerator._GenerateTorches();
+            enemiesSpawner._SpawnEnemies();
             playerSpawner.Spawn();
         }
     }
