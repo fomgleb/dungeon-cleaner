a8e21e7eaf6dd00c655cb27de098edf23ef160f5
fomgleb <fomglib@gmail.com>
2023-02-15 11:48:57 +0300

    Move a part of aim appearing logic to DungeonInit

--- Assets/Game/Dungeon/Scripts/DungeonInit.cs
+++ Assets/Game/Dungeon/Scripts/DungeonInit.cs
@@ -1,21 +1,27 @@
 using Game.Pause;
 using Game.UI.Scripts;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Game.Dungeon.Scripts
 {
     public class DungeonInit : MonoBehaviour
     {
+        [SerializeField] private AudioMixerSnapshot inAimTipAudioMixerSnapshot;
+
         [SerializeField] private DungeonMusicPlayer dungeonMusicPlayer;
         [SerializeField] private DungeonGeneratorBase caveGenerator;
         [SerializeField] private RandomTorchesInDungeonGenerator torchesGenerator;
         [SerializeField] private RandomEnemiesSpawner enemiesSpawner;
         [SerializeField] private GameObjectSpawner playerSpawner;
         [SerializeField] private SlimesCounter slimesCounter;
+        [SerializeField] private Window aimWindow;
 
         private void Start()
         {
             Pauser.ClearRegisteredHandlers();
+
+            inAimTipAudioMixerSnapshot.TransitionTo(0);
 
             dungeonMusicPlayer.PlayRandomMusic();
             caveGenerator._GenerateDungeon();
@@ -23,6 +29,9 @@ namespace Game.Dungeon.Scripts
             enemiesSpawner._SpawnEnemies();
             playerSpawner.Spawn();
             slimesCounter.ShowEnemiesCount(enemiesSpawner.SpawnedEnemies.Count);
+            aimWindow.Show();
+
+            Pauser.SetPaused(true);
         }
     }
 }
