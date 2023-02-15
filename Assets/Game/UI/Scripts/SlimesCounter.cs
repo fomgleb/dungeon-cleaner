71a2a43fff8f7b3be94e8f1870efbf136b30ba86
fomgleb <fomglib@gmail.com>
2023-02-15 21:19:37 +0300

    Add NumberOfEnemiesChangedEvent.cs

--- Assets/Game/UI/Scripts/SlimesCounter.cs
+++ Assets/Game/UI/Scripts/SlimesCounter.cs
@@ -1,5 +1,3 @@
-using System.Collections.Specialized;
-using Game.Dungeon.Scripts;
 using TMPro;
 using UnityEngine;
 
@@ -8,22 +6,6 @@ namespace Game.UI.Scripts
     public class SlimesCounter : MonoBehaviour
     {
         [SerializeField] private TMP_Text text;
-        [SerializeField] private RandomEnemiesSpawner enemiesSpawner;
-
-        private void OnEnable()
-        {
-            enemiesSpawner.SpawnedEnemies.CollectionChanged += OnEnemiesCollectionChanged;
-        }
-
-        private void OnDisable()
-        {
-            enemiesSpawner.SpawnedEnemies.CollectionChanged -= OnEnemiesCollectionChanged;
-        }
-
-        private void OnEnemiesCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            ShowEnemiesCount(enemiesSpawner.SpawnedEnemies.Count);
-        }
 
         public void ShowEnemiesCount(int count)
         {
