64b40541868bea5e2e588cbfb33b8e67680a3040
fomgleb <fomglib@gmail.com>
2023-02-15 11:48:57 +0300

    Move a part of aim appearing logic to DungeonInit

--- Assets/GameUIAndHUDController.cs
+++ Assets/GameUIAndHUDController.cs
@@ -4,18 +4,9 @@ using UnityEngine.Audio;
 
 public class GameUIAndHUDController : MonoBehaviour
 {
-    [SerializeField] private GameObject health;
-    [SerializeField] private GameObject slimesCounter;
     [SerializeField] private GameObject aimMessage;
     [SerializeField] private AudioMixerSnapshot normal;
-    [SerializeField] private AudioMixerSnapshot inAimTip;
     
-    private void Start()
-    {
-        TurnAimMessage(true);
-        inAimTip.TransitionTo(0);
-    }
-
     private void Update()
     {
         if (Input.anyKey)
@@ -28,8 +19,6 @@ public class GameUIAndHUDController : MonoBehaviour
     private void TurnAimMessage(bool state)
     {
         Pauser.SetPaused(state);
-        health.SetActive(!state);
-        slimesCounter.SetActive(!state);
         aimMessage.SetActive(state);
     }
 }
