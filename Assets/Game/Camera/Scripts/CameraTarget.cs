f16373ce813399424ea942b78523b9a43c36bb4f
fomgleb <fomglib@gmail.com>
2023-02-15 18:53:36 +0300

    Add PlayerSpawnedEvent.cs

--- Assets/Game/Camera/Scripts/CameraTarget.cs
+++ Assets/Game/Camera/Scripts/CameraTarget.cs
@@ -1,6 +1,5 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
-using Game.Dungeon.Scripts;
 using Game.Mouse.Scripts;
 using UnityEngine;
 
@@ -9,9 +8,6 @@ namespace Game.Camera.Scripts
     public class CameraTarget : MonoBehaviour
     {
         [Range(2, 100)] [SerializeField] private float cameraTargetDivider;
-        [SerializeField] private GameObjectSpawner playerSpawner;
-
-        private Transform playerTransform;
 
         private CancellationTokenSource lookingAtMouseCancellationToken;
 
@@ -20,27 +16,18 @@ namespace Game.Camera.Scripts
             lookingAtMouseCancellationToken = new CancellationTokenSource();
         }
 
-        private void OnEnable() => playerSpawner.SpawnedEvent += OnPlayerSpawned;
-        private void OnDisable() => playerSpawner.SpawnedEvent -= OnPlayerSpawned;
-
         private void OnDestroy()
         {
             lookingAtMouseCancellationToken.Cancel();
         }
 
-        private void OnPlayerSpawned()
-        {
-            playerTransform = playerSpawner.SpawnedObject.transform;
-            LookAtMouseAsync(lookingAtMouseCancellationToken.Token);
-        }
-
         public void StopLookingAtMouse() => lookingAtMouseCancellationToken.Cancel();
 
-        private async void LookAtMouseAsync(CancellationToken token)
+        public async void LookAtMouseAsync(Transform playerTransform)
         {
             while (true)
             {
-                if (token.IsCancellationRequested) return;
+                if (lookingAtMouseCancellationToken.IsCancellationRequested) return;
 
                 var cameraTargetPosition = ((Vector3)MouseLocation.WorldPosition + (cameraTargetDivider - 1) *
                     playerTransform.position) / cameraTargetDivider;
