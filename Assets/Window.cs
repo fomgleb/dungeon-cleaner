6b262889444ce158cc7aa3bf097b3c63e4a1a99c
fomgleb <fomglib@gmail.com>
2023-02-15 15:11:28 +0300

    Upgrade Window.cs

--- Assets/Window.cs
+++ Assets/Window.cs
@@ -1,22 +1,41 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class Window : MonoBehaviour
 {
+    [SerializeField] private float appearanceTime;
+
     private Transform[] childrenTransforms;
+    private CanvasGroup canvasGroup;
 
     private void Awake()
     {
         childrenTransforms = GetComponentsInChildren<Transform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    public void Show()
+    public async void ShowAsync()
     {
         foreach (var childrenObject in childrenTransforms)
             childrenObject.gameObject.SetActive(true);
+
+        if (appearanceTime != 0)
+        {
+            var appearanceSpeed = 1 / appearanceTime;
+            for (var elapsedTime = 0f; elapsedTime < appearanceTime; elapsedTime += Time.deltaTime)
+            {
+                canvasGroup.alpha += appearanceSpeed * Time.deltaTime;
+                await UniTask.Yield();
+            }
+        }
+
+        canvasGroup.alpha = 1;
     }
 
     public void Hide()
     {
+        canvasGroup.alpha = 0;
         foreach (var childrenTransform in childrenTransforms)
             childrenTransform.gameObject.SetActive(false);
     }
